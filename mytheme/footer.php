f31d3c2204d97b57e32be8c025f0869079636e52
VeroniqueDelauney <veronique.delauney@strategikweb.fr>
2024-06-03 14:25:14 +0200

    Créations du thème enfant Mytheme

---
+++ mytheme/footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package nathaliemota
+ */
+
+?>
+
+	<footerclass="site-footer">
+            <div>
+                  Footer content comes here
+            </div>
+	</footer>
+</div>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
